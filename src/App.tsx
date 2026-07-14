d3dd548e2d0295e431254754b7600901620f7ce0
KEX-03 <killex003@outlook.com>
2026-07-14 11:03:24 +0530

    implemented delete using `dispatch()`

--- src/App.tsx
+++ src/App.tsx
@@ -18,6 +18,10 @@ function reducer(todos, action) {
         }
         return todo
       })
+    case ACTIONS.DELETE_TODO:
+      return todos.filter(todo => todo.id !== action.payload.id)
+    default:
+      return todos
   }
 }
 
